b9c083b5eb822cb3a0579123f50dc836bc0d0995
Ayelet41 <problemsolver@google.com>
2021-07-06 14:33:20 +0100

    Revert "format+ version"
    This reverts commit 739e636d8207ef0d0a64f2cad59cf670ed0af24c.

--- terraform/03_gke_cluster.tf
+++ terraform/03_gke_cluster.tf
@@ -56,9 +56,6 @@ resource "google_container_cluster" "gke" {
   workload_identity_config {
     identity_namespace = "${data.google_project.project.project_id}.svc.id.goog"
   }
-  resource_labels = {
-    "version" = var.app_version
-  }
 
   # Using an embedded resource to define the node pool. Another
   # option would be to create the node pool as a separate resource and link it
